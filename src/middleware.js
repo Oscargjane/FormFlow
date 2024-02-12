8724936bb41a4d3df7a9ea47a3c300b0bdf31921
Oscar Jane <hello@oscar-jane.com>
2024-02-12 20:26:55 +0100

    feat: mejora de las redirecciones con middleware de Next.js y Clerk
    - Mejoradas las redirecciones utilizando un middleware de Next.js y métodos de Clerk, el servicio de autenticación.
    - Agregada redirección permanente de Next.js.

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,42 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
 
-// Este middleware se aplica a todas las rutas de tu aplicación.
-// Las rutas especificadas en `publicRoutes` no requerirán autenticación.
-// Todas las demás rutas requerirán que el usuario esté autenticado.
+/**
+ * Middleware de autenticación para Next.js utilizando Clerk.
+ * Este middleware se aplica a todas las rutas de la aplicación.
+ * Las rutas especificadas en `publicRoutes` no requerirán autenticación.
+ * Todas las demás rutas requerirán que el usuario esté autenticado.
+ */
 export default authMiddleware({
+  // La función `afterAuth` se ejecuta después de que el middleware de autenticación ha procesado la solicitud.
+  afterAuth(auth, req) {
+    // Comprueba si la ruta actual es una de las rutas de inicio de sesión o registro.
+    const isSignInOrSignUpRoute = ['/sign-in', '/sign-up'].includes(req.nextUrl.pathname);
+
+    // Si el usuario no está autenticado y la ruta no es pública, redirige al usuario a la página de inicio de sesión
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    // Si el usuario está autenticado y está en una ruta de inicio de sesión o registro, redirige al usuario al dashboard
+    if (auth.userId && isSignInOrSignUpRoute) {
+      const redirectUrl = new URL('/dashboard', req.url);
+      // Añade ?from=/ruta-entrante a la URL del dashboard
+      redirectUrl.searchParams.set('from', req.nextUrl.pathname);
+      // Y redirige a la nueva URL
+      return NextResponse.redirect(redirectUrl);
+    }
+
+    // Si el usuario está autenticado o la ruta es pública, permite que la solicitud continúe
+    if (auth.userId || auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+
+    // En otras situaciones (por ejemplo, un usuario autenticado intentando acceder a una ruta no pública),
+    // redirige al usuario a la página de inicio de sesión
+    return redirectToSignIn({ returnBackUrl: req.url });
+  },
   publicRoutes: ['/', '/api/webhook/clerk'],
-  ignoredRoutes: ['/api/webhook/clerk'],
 });
 
 // `config` define cómo se aplica el middleware.
