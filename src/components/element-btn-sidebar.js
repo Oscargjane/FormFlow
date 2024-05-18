5067ecf170547d4b21ff799f18c5c8e90a44bb62
Oscar Jane <hello@oscar-jane.com>
2024-05-18 09:35:09 +0200

    refactor: refactor and optimize the loading of the editor buttons in the Sidebar
    This commit makes improvements to the editor buttons in the Sidebar:
    
    - The loading of the individual buttons and the set of buttons was refactored and optimized using React.memo. This should improve performance by avoiding unnecessary renders.
    - Support for new form elements was added. This expands the capabilities of the editor and allows for greater flexibility.
    - The styles were improved for better visual consistency and a better user experience.

--- src/components/element-btn-sidebar.js
+++ src/components/element-btn-sidebar.js
@@ -2,16 +2,16 @@ import { Button } from '@/components/ui/button.js';
 import { useDraggable } from '@dnd-kit/core';
 import { cn } from '@/lib/utils';
 import Icon from '@/components/ui/icon.js';
+import { memo } from 'react';
 
-/**
- * SidebarBtnElement es un componente que representa un botón en la barra lateral que puede ser arrastrado.
- * Recibe un objeto formElement como prop, que contiene la información del elemento del formulario que este botón representa.
- */
-function SidebarBtnElement({ formElement }) {
-  // se extrae el icono y la label del elemento del formulario
+const buttonClass =
+  'bg-white rounded-md cursor-pointer border border-white shadow hover:shadow-md hover:shadow-neutral-400/50 hover:bg-white min-w-10 min-h-20 py-1 transition-shadow duration-200';
+const textClass =
+  'text-neutral-700 text-xs font-medium h-full flex justify-center text-center leading-4 items-center overflow-hidden overflow-ellipsis whitespace-normal';
+
+const SidebarBtnElement = memo(({ formElement }) => {
   const { icon, label } = formElement.editorBtnElement;
 
-  // configuracion del boton para ser arrastrable
   const draggable = useDraggable({
     id: `editor-btn----${formElement.type}`,
     data: {
@@ -25,10 +25,7 @@ function SidebarBtnElement({ formElement }) {
       key={formElement.type}
       ref={draggable.setNodeRef}
       variant={'outline'}
-      className={cn(
-        'bg-white rounded-md cursor-pointer border border-white shadow hover:shadow-md hover:shadow-neutral-400/50 min-w-10 min-h-20',
-        draggable.isDragging && 'ring-2 ring-blue-500',
-      )}
+      className={cn(buttonClass, draggable.isDragging && 'ring-2 ring-blue-500')}
       {...draggable.listeners}
       {...draggable.attributes}
     >
@@ -36,26 +33,10 @@ function SidebarBtnElement({ formElement }) {
         <div className="p-[0.5px] rounded bg-neutral-50 border-[0.5px]">
           <Icon name={icon} className="h-6 w-6" />
         </div>
-        <p className="text-neutral-700 text-xs font-medium flex justify-center  text-center leading-3 h-6 items-center">
-          {label}
-        </p>
+        <p className={textClass}>{label}</p>
       </div>
     </Button>
   );
-}
-
-// export function SidebarBtnElementDragOverlay({ formElement = FormElements }) {
-//   const { label, icon } = formElement.designerBtnElement;
-
-//   return (
-//     <Button
-//       variant={'outline'}
-//       className="flex flex-col gap-2 h-[120px] w-[120px] cursor-grab"
-//     >
-//       <Icon className="h-8 w-8 text-primary cursor-grab" />
-//       <p className="text-xs">{label}</p>
-//     </Button>
-//   );
-// }
+});
 
 export default SidebarBtnElement;
--- src/components/form-elements-sidebar.js
+++ src/components/form-elements-sidebar.js
@@ -1,26 +1,42 @@
+import React from 'react';
 import SidebarBtnElement from '@/components/element-btn-sidebar.js';
 import { FormElements } from '@/components/form-elements.js';
 
-const formElementsList = [
-  FormElements.TitleField,
-  FormElements.ParagraphField,
-  FormElements.DividerField,
-  FormElements.MultipleChoiceField,
-];
+const formElementsList = {
+  'Display & Layout': [
+    FormElements.TitleField,
+    FormElements.ParagraphField,
+    FormElements.DividerField,
+  ],
+  Choices: [FormElements.MultipleChoiceField],
+  Text: [FormElements.ShortAnswerField, FormElements.LongAnswerField],
+};
+
+const FormElementsGroup = ({ category, formElements }) => {
+  return (
+    <div className="flex flex-col" key={category}>
+      <p className="text-sm text-neutral-400 font-medium mt-6">{category}</p>
+      <div className="grid grid-cols-3 gap-2 mt-3 gap-y-4">
+        {formElements.map((formElement) => (
+          <SidebarBtnElement key={formElement.type} formElement={formElement} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 function FormElementsSidebar() {
   return (
     <aside className="w-full h-full bg-neutral-50 flex flex-col border-r border-neutral-300  pt-4 px-3 pb-6 max-w-[300px] min-w-[270px]">
-      <div className="flex flex-col">
-        <p className="text-sm text-neutral-400 font-medium mt-6">Display & Layout</p>
-        <div className="grid grid-cols-3 gap-2 mt-3 gap-y-4">
-          {formElementsList.map((formElement) => (
-            <SidebarBtnElement key={formElement.type} formElement={formElement} />
-          ))}
-        </div>
-      </div>
+      {Object.entries(formElementsList).map(([category, formElements]) => (
+        <FormElementsGroup
+          key={category}
+          category={category}
+          formElements={formElements}
+        />
+      ))}
     </aside>
   );
 }
 
-export default FormElementsSidebar;
+export default React.memo(FormElementsSidebar);
